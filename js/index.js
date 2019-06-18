7fdf5c6cd080c4804703ba3862c6f3dbfcde611d
Connor Claxton <czclaxton@gmail.com>
2019-06-18 14:33:17 -0500

    added mouseover and mouseleave events, also added click event

--- js/index.js
+++ js/index.js
@@ -1,7 +1,19 @@
 // Your code goes here
-let bgcolor;
-let button;
-function setup( {
+// let hoverEvent = document.getElementsByClassName('nav-link');
+// hoverEvent.onclick = function()
 
+let bgcolor = ["#e74c3c","#3498db","#2ecc71","#95a5a6","#f1c40f","#e74c3c"]
+let i = 0;
+document.querySelector("button").addEventListener("click", function(){
+    i = i < bgcolor.length ? ++i : 0;
+document.querySelector("html").style.background = bgcolor[i]
 })
 
+let imgMouseOver = document.getElementById('funBus');
+imgMouseOver.addEventListener("mouseover", function() {
+    this.src="img/new-fun-bus.jpg";
+})
+
+imgMouseOver.addEventListener("mouseleave", function() {
+    this.src="img/fun-bus.jpg";
+})
